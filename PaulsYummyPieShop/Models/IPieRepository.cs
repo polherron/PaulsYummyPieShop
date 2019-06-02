652b7f7b1ceb2665d63b1104504ceba26b3cc378
polherron <pol.herron@gmail.com>
2019-06-02 12:45:35 +0100

    Add Model Repositories

---
+++ PaulsYummyPieShop/Models/IPieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaulsYummyPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+
+        Pie GetPieById(int id);
+    }
+}
+
+
