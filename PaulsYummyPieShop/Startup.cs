3126be664115b2986bed80bfe9b1f21fc532acae
polherron <pol.herron@gmail.com>
2019-06-02 15:56:19 +0100

    Add Transient

--- PaulsYummyPieShop/Startup.cs
+++ PaulsYummyPieShop/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using PaulsYummyPieShop.Models;
 
 namespace PaulsYummyPieShop
 {
@@ -16,6 +17,7 @@ namespace PaulsYummyPieShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<IPieRepository, MockPieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
