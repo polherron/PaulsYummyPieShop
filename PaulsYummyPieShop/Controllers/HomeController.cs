7b0472fefb0cea3447b4864b72d74263cd8d3bf3
polherron <pol.herron@gmail.com>
2019-06-02 13:12:22 +0100

    Added the controllerl

---
+++ PaulsYummyPieShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaulsYummyPieShop.Models;
+
+namespace PaulsYummyPieShop.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        //Within the constructor, dependency injection is used
+        // to instantiate the pieRepository object
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        //Get: /Controller
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
